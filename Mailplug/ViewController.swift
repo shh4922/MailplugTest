09f4a77eb6b1136f883ee8566ed44ce3a81b74e6
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2023-10-23 10:54:06 +0900

    Initial Commit

---
+++ Mailplug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mailplug
+//
+//  Created by Hyeonho on 10/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
